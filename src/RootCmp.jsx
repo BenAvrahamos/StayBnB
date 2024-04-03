9b67ab7a9c5bb45057b5ca2e7003c3380a0228e9
BenAvrahamos <benavraham1998@gmail.com>
2024-04-03 20:38:40 +0300

    changed script name at index html

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -3,17 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import './style/main.css'
 
-import { StayIndex } from './views/StayIndex'
-import { StayDetails } from './views/StayDetails'
+// import { StayIndex } from './views/StayIndex'
+// import { StayDetails } from './views/StayDetails'
 
 export function RootCmp() {
-  return <Provider>
-    <Router>
-      <Routes>
-        <Route path='/' element={<StayIndex/>}/>
-        <Route path='/:stayId' element={<StayDetails/>}/>
-      </Routes>
-    </Router>
-  </Provider>
+  return 
+    
+//     <Router>
+//       <Routes>
+//         {/* <Route path='/' element={<StayIndex/>}/> */}
+//         {/* <Route path='/:stayId' element={<StayDetails/>}/> */}
+//       </Routes>
+//     </Router>
+ 
 
 }
