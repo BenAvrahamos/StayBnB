ea66bd97bd75bd36ffca205b49459d510e473fa3
BenAvrahamos <benavraham1998@gmail.com>
2024-04-04 00:02:18 +0300

    Fixed appheader issue

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -4,9 +4,12 @@ import './style/main.css'
 
 import { StayIndex } from './views/StayIndex'
 import { StayDetails } from './views/StayDetails'
+import { AppHeader } from './views/AppHeader'
 
 export function RootCmp() {
   return <Router>
+    <AppHeader/>
+
     <Routes>
       <Route path='/' element={<StayIndex />} />
       <Route path='/:stayId' element={<StayDetails />} />
