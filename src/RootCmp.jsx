7449fe2e91008e9e54a619b466f0fab44ee1a856
BenAvrahamos <benavraham1998@gmail.com>
2024-04-03 20:46:05 +0300

    Fixed rootcmp

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -2,24 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import './style/main.css'
 
-// import { StayIndex } from './views/StayIndex'
-// import { StayDetails } from './views/StayDetails'
+import { StayIndex } from './views/StayIndex'
+import { StayDetails } from './views/StayDetails'
 
 export function RootCmp() {
   return <Router>
-      <Routes>
-        <Route path='/' element={<StayIndex/>}/>
-        <Route path='/:stayId' element={<StayDetails/>}/>
-      </Routes>
-    </Router>
-  return 
-    
-//     <Router>
-//       <Routes>
-//         {/* <Route path='/' element={<StayIndex/>}/> */}
-//         {/* <Route path='/:stayId' element={<StayDetails/>}/> */}
-//       </Routes>
-//     </Router>
- 
-
+    <Routes>
+      <Route path='/' element={<StayIndex />} />
+      <Route path='/:stayId' element={<StayDetails />} />
+    </Routes>
+  </Router>
 }
