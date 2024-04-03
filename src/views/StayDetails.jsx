33202a91c1f5e9900a07521492e5b304433b8130
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-03 22:45:58 +0300

    Add basic header of page

--- src/views/StayDetails.jsx
+++ src/views/StayDetails.jsx
@@ -1,3 +1,32 @@
+import { useParams } from "react-router"
+import { useState, useEffect } from 'react'
+import { stayService } from "../services/stay.local.service"
+
 export function StayDetails() {
-    return <h1>hello</h1>
+    const { stayId } = useParams()
+    const [stay, setStay] = useState('')
+
+    useEffect(() => {
+        if (stayId) loadStay()
+    }, [])
+
+    async function loadStay() {
+        try {
+            const stay = await stayService.getById(stayId)
+            setStay(stay)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    return (
+        <section className='stay-details'>
+            <div className="stay-details-header">
+                <h1>{stay.summary}</h1>
+                <span></span><button>Share</button>
+                <span></span><button>Save</button>
+            </div>
+
+        </section>
+    )
 }
