f242e136748d235b8d325de8a326d7f63222ace3
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-04 23:25:09 +0300

    Add styling properties and edit cmp

--- src/pages/StayDetails.jsx
+++ src/pages/StayDetails.jsx
@@ -55,9 +55,9 @@ export function StayDetails() {
                     <div className="header-btns flex">
                         <button className="share flex">
                             <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style={{display: 'block', fill: 'none', height: '16px', width: '16px', stroke: 'currentcolor', strokeWidth: '2', overflow: 'visible'}}><g fill="none"><path d="M27 18v9a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-9M16 3v23V3zM6 13l9.3-9.3a1 1 0 0 1 1.4 0L26 13"></path></g></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style={{ display: 'block', fill: 'none', height: '16px', width: '16px', stroke: 'currentcolor', strokeWidth: '2', overflow: 'visible' }}><g fill="none"><path d="M27 18v9a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-9M16 3v23V3zM6 13l9.3-9.3a1 1 0 0 1 1.4 0L26 13"></path></g></svg>
                             </span><span>Share</span></button>
-                        <button className="save flex"><span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style={{display: 'block', fill: 'none', height: '16px', width: '16px', stroke: 'currentcolor', strokeWidth: 2, overflow: 'visible'}}><path d="M16 28c7-4.73 14-10 14-17a6.98 6.98 0 0 0-7-7c-1.8 0-3.58.68-4.95 2.05L16 8.1l-2.05-2.05a6.98 6.98 0 0 0-9.9 0A6.98 6.98 0 0 0 2 11c0 7 7 12.27 14 17z"></path></svg></span><span>Save</span></button>
+                        <button className="save flex"><span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style={{ display: 'block', fill: 'none', height: '16px', width: '16px', stroke: 'currentcolor', strokeWidth: 2, overflow: 'visible' }}><path d="M16 28c7-4.73 14-10 14-17a6.98 6.98 0 0 0-7-7c-1.8 0-3.58.68-4.95 2.05L16 8.1l-2.05-2.05a6.98 6.98 0 0 0-9.9 0A6.98 6.98 0 0 0 2 11c0 7 7 12.27 14 17z"></path></svg></span><span>Save</span></button>
                     </div>
                 </div>
                 <StayGalleryPreview stay={stay} />
@@ -70,14 +70,14 @@ export function StayDetails() {
                             <p>★ No reviews yet</p>
                             <hr />
                         </div>
-                        <div className="hosted-by">
-                            <img src={stay.host.imgUrl} className="host-img"/>
-                            <div className="hosted-by-txt">
+                        <div className="host-by flex">
+                            <img src={stay.host.imgUrl} className="host-img" />
+                            <div className="host-by-txt flex column">
                                 <h3>Hosted by {_findHostName()}</h3>
-                                {/* <p>{stay.host.experience.isSuper ? 'Superhost' : '0'} ・ {stay.host.experience.hostingTime > 1 ? 'years' : 'year'} hosting</p> */}
+                                <p>{stay.host.experience.isSuper ? 'Superhost ・' : ''}  {stay.host.experience.hostingTime > 1 ? `${stay.host.experience.hostingTime} years` : 'year'} hosting</p>
                             </div>
-                            <hr />
                         </div>
+                        <hr />
         //if there is a special policy to stay - add policy cmp. now we do not have it in data
                         {/* <div className="stay-rooms">
                     <h1>Where you'll sleep</h1>
