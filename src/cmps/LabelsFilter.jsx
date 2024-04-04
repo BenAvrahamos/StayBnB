e021454704f02a4ea53568eb98576d24279e72ec
JennyLottner <jenny.lottner@gmail.com>
2024-04-04 20:14:02 +0300

    Change labels filter cmp name

--- src/cmps/LabelsFilter.jsx
+++ src/cmps/LabelsFilter.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 
 import { SvgCmp } from "./SvgCmp"
 
-export function LabelFilter({ onSetFilter, filterBy }) {
+export function LabelsFilter({ onSetFilter, filterBy }) {
 	const [filterByToEdit, setFilterByToEdit] = useState(filterBy)
 
 	const filterLabels = ['iconic_cities', 'new', 'off-the-grid', 'rooms', 'creative_spaces', 'boats', 'grand_pianos', 'vineyards', 'historical_homes', 'mansions', 'lake', 'bed_&_breakfasts', 'treehouses', 'farms', 'skiing', 'earth_homes', 'countryside', 'amazing_views', 'beach', 'desert', 'a-frames',
