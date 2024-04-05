f2afd3b08842d81f03784def199cc334297bb4f9
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-05 21:28:37 +0300

    Add cmp with basic functionality

---
+++ src/cmps/StayDetailsCmps/StayGalleryPreview.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+export function StayGalleryPreview({ stay }) {
+    const [fivePicsPreview, setFivePicsPreview] = useState([])
+
+    useEffect(() => {
+        if (stay.imgUrls.length >= 5) setFivePicsPreview(stay.imgUrls.slice(0, 5))
+    }, [])
+
+    return (
+        <section className="gallery-stay-details">
+            {/* {fivePicsPreview && fivePicsPreview.map((pic,idx) => <img key={idx} src={pic} className={`img${idx}`} />)} */}
+        </section>
+    )
+}
