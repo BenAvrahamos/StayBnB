c8924fb33cb5f3849039d07ffda98c88247fdb31
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-05 21:24:56 +0300

    Add more features and add styling properties dynamically

--- src/pages/StayDetails.jsx
+++ src/pages/StayDetails.jsx
@@ -1,14 +1,16 @@
 import { useParams } from "react-router"
 import { useState, useEffect } from 'react'
 import { stayService } from "../services/stay.local.service"
-import { StayGalleryPreview } from '../cmps/StayGalleryPreview'
-import { StayReserveModal } from '../cmps/StayReserveModal'
-// import { stayDetailsSvg } from "../assets/svg/stay-details-svg/stay-details-svg"
+import { StayGalleryPreview } from '../cmps/StayDetailsCmps/StayGalleryPreview'
+import { StayReserveModal } from '../cmps/StayDetailsCmps/StayReserveModal'
+import { StayDetailsSvg } from "../cmps/StayDetailsCmps/StayDetailsSvg"
+import { BedroomDetails } from '../cmps/StayDetailsCmps/BedroomDetails'
 
 export function StayDetails() {
     const safetyAmenities = ['Carbon monoxide alarm', 'Smoke alarm']
     const { stayId } = useParams()
     const [stay, setStay] = useState('')
+    const [longestBedsArrCount, setLongestBedsArrCount] = useState(1)
 
     // is guest favorite - if truthy - show a cmp of guest fav
     useEffect(() => {
@@ -16,6 +18,23 @@ export function StayDetails() {
             loadStay()
         }
     }, [])
+
+    useEffect(() => {
+        if (stay) {
+          calcLongestBedCount();
+        }
+      }, [stay])
+
+    function calcLongestBedCount() {
+        let maxBedCount = 0
+        stay.bedrooms.forEach((bedroom) => {
+          if (bedroom.beds.length > maxBedCount) {
+            maxBedCount = bedroom.beds.length
+          }
+        })
+        maxBedCount-- // two rows can contain up to 3 types of beds.
+        setLongestBedsArrCount(maxBedCount)
+      }
 
     function countBedsInBedrooms() {
         const numOfBeds = stay.bedrooms.reduce((acc, bedroomObj) => {
@@ -34,11 +53,6 @@ export function StayDetails() {
         }
     }
 
-    // function _checkForSafetyAmenities() {
-    //     let issafetyAmenities = stay.amenities.map(amenity => safetyAmenities.filter(safetyAmenity => safetyAmenity === amenity))
-    //     return safetyAmenities
-    // }
-
     function _findHostName() {
         const host = stay.host
         const spaceIdx = host.fullName.indexOf(' ')
@@ -50,24 +64,23 @@ export function StayDetails() {
         <>
             {stay && <section className='stay-details'>
                 <div className="stay-details-header flex space-between align-center">
-                    {/* {stayDetailsSvg.couch} */}
                     <h1>{stay.summary}</h1>
                     <div className="header-btns flex">
                         <div className="first-btn">
-                        <button className="share flex">
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style={{ display: 'block', fill: 'none', height: '16px', width: '16px', stroke: 'currentcolor', strokeWidth: '2', overflow: 'visible' }}><g fill="none"><path d="M27 18v9a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-9M16 3v23V3zM6 13l9.3-9.3a1 1 0 0 1 1.4 0L26 13"></path></g></svg>
-                            </span>
-                            <span>Share</span>
-                        </button>
+                            <button className="share flex">
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style={{ display: 'block', fill: 'none', height: '16px', width: '16px', stroke: 'currentcolor', strokeWidth: '2', overflow: 'visible' }}><g fill="none"><path d="M27 18v9a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-9M16 3v23V3zM6 13l9.3-9.3a1 1 0 0 1 1.4 0L26 13"></path></g></svg>
+                                </span>
+                                <span>Share</span>
+                            </button>
                         </div>
                         <div className="second-btn flex">
-                        <button className="save flex">
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style={{ display: 'block', fill: 'none', height: '16px', width: '16px', stroke: 'currentcolor', strokeWidth: 2, overflow: 'visible' }}><path d="M16 28c7-4.73 14-10 14-17a6.98 6.98 0 0 0-7-7c-1.8 0-3.58.68-4.95 2.05L16 8.1l-2.05-2.05a6.98 6.98 0 0 0-9.9 0A6.98 6.98 0 0 0 2 11c0 7 7 12.27 14 17z"></path></svg>
-                            </span>
-                            <span>Save</span>
-                        </button>
+                            <button className="save flex">
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style={{ display: 'block', fill: 'none', height: '16px', width: '16px', stroke: 'currentcolor', strokeWidth: 2, overflow: 'visible' }}><path d="M16 28c7-4.73 14-10 14-17a6.98 6.98 0 0 0-7-7c-1.8 0-3.58.68-4.95 2.05L16 8.1l-2.05-2.05a6.98 6.98 0 0 0-9.9 0A6.98 6.98 0 0 0 2 11c0 7 7 12.27 14 17z"></path></svg>
+                                </span>
+                                <span>Save</span>
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -89,26 +102,32 @@ export function StayDetails() {
                             </div>
                         </div>
                         <hr />
-        //if there is a special policy to stay - add policy cmp. now we do not have it in data
-                        {/* <div className="stay-rooms">
-                    <h1>Where you'll sleep</h1>
-                    {stay.bedrooms.map(bedroom => {
-                        <div>
-
-                        </div>
-                    })}
-                </div> */}
-                        <div className="amenities-of-stay"> // grid with two columns. first col - 5 first amenities, sec col - 5 others.
-                            <h1>What this place offers: </h1>
-                            {/* <span>{stayDetailsSvg.couch.replaceAll('`', '')</span> */}
-                            <ul className="first-col-amenity-ul">
-                                {stay.amenities.slice(0, 3).map(amenity => <li key={amenity}>{amenity}</li>)}
-                                <li className={stay.amenities.includes(safetyAmenities[0]) ? '' : 'no-safety-amenity'}>{safetyAmenities[0]}</li>
-                            </ul>
-                            <ul>
-                                {stay.amenities.slice(4, 9).map(amenity => <li key={amenity}>{amenity}</li>)}
-                                <li className={stay.amenities.includes(safetyAmenities[1]) ? '' : 'no-safety-amenity'}>{safetyAmenities[1]}</li>
-                            </ul>
+                        <div className="stay-rooms">
+                            <h2>Where you'll sleep</h2>
+                            <div className="bedroom-container grid">
+                                {stay.bedrooms.map(bedroom => {
+                                    const bedsLength = bedroom.beds.length
+                                    return <div className="bedroom-div" key={bedroom.name} style={{paddingInline: '1rem', paddingBlockStart: '1rem',
+                                     paddingBlockEnd: bedsLength < longestBedsArrCount ? ((longestBedsArrCount - bedsLength ) * .875 ) + 1.5 + 'rem' : '1.5rem'}}>
+                                        <div className="icons flex align-center">
+                                            {bedroom.beds.map((bed, idx) => <StayDetailsSvg bed={bed} key={`${bed}${idx}`} />)}
+                                        </div>
+                                        <h4>{bedroom.name}</h4>
+                                        <BedroomDetails beds={bedroom.beds} />
+                                    </div>
+                                })}
+                            </div>
+                            <div className="amenities-of-stay">
+                                <h1>What this place offers: </h1>
+                                <ul className="first-col-amenity-ul">
+                                    {stay.amenities.slice(0, 3).map(amenity => <li key={amenity}>{amenity}</li>)}
+                                    <li className={stay.amenities.includes(safetyAmenities[0]) ? '' : 'no-safety-amenity'}>{safetyAmenities[0]}</li>
+                                </ul>
+                                <ul> // grid with two columns. first col - 5 first amenities, sec col - 5 others.
+                                    {stay.amenities.slice(4, 9).map(amenity => <li key={amenity}>{amenity}</li>)}
+                                    <li className={stay.amenities.includes(safetyAmenities[1]) ? '' : 'no-safety-amenity'}>{safetyAmenities[1]}</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
