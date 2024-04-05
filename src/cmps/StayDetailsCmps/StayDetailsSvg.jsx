70fe5fb7962105c2e52f33cd9f7f2b0d41a0b0d5
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-05 16:45:36 +0300

    Add cmp to svg for staydetails page

---
+++ src/cmps/StayDetailsCmps/StayDetailsSvg.jsx
@@ -0,0 +1,18 @@
+import { stayDetailsSvgService } from '../../services/stay-details-svg'
+import { StringToSvg } from "../StringToSvg"
+
+export function StayDetailsSvg({ bed }) {
+    console.log(bed)
+    if (bed === 'couch' || bed === 'sofa bed') {
+        return <span className={`icon ${bed}`} key={bed}><StringToSvg svgString={stayDetailsSvgService.sofaBed} /></span>
+    }
+    if (bed === 'double bed') {
+        return <span className={`icon ${bed}`} key={bed}><StringToSvg svgString={stayDetailsSvgService.doubleBed} /></span>
+    }
+    if (bed === 'single bed') {
+        return <span className={`icon ${bed}`} key={bed}><StringToSvg svgString={stayDetailsSvgService.singleBed} /></span>
+    }
+    if (bed === 'king size bed' || bed === 'queen size bed') {
+        return <span className={`icon ${bed}`} key={bed}><StringToSvg svgString={stayDetailsSvgService.queenKingBed} /></span>
+    }
+}
