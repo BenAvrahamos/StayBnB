050073a163aaba580af6800ff12ff94df004f10e
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-06 18:20:13 +0300

    Edit reducerfor demo data to stay details page

--- src/store/reducers/reservation.reducer.js
+++ src/store/reducers/reservation.reducer.js
@@ -1,16 +1,18 @@
 export const UPDATE_RESERVATION = 'UPDATE_RESERVATION'
+import { startOfDay, addDays } from 'date-fns'
+
+// DEMO DATA TILL GETTING INFO FOR RESERVATION FROM PARAMS
+const todayTimestamp = startOfDay(new Date()).getTime()
+// Timestamp for today + three days
+const timestampThreeDaysLater = addDays(todayTimestamp, 3).getTime()
 
 // DEMO DATA FOR NOW
-const checkout = new Date()
-const checkIn = new Date(checkout)
-checkIn.setDate(new Date(checkIn.getDate() - 1))
 
 const initialState = {
     reservation: {
-        checkIn: checkIn,
-        checkout: checkout,
-        guests: {adults: 1, children: 0, infants: 0, pets: 0, sum: 1},
-        isClickedForBooking: false
+        checkIn: todayTimestamp,
+        checkout: timestampThreeDaysLater,
+        guests: {adults: 1, children: 0, infants: 0, pets: 0, sum: 1}
     }
 }
 
