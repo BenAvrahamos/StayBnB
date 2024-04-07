c4407c06d9df2326b068fafb4a398de1cf0fc545
JennyLottner <jenny.lottner@gmail.com>
2024-04-07 12:28:38 +0300

    Send work-in-progress to collaborator

--- src/cmps/LabelsFilter.jsx
+++ src/cmps/LabelsFilter.jsx
@@ -1,21 +1,16 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 import { SvgCmp } from "./SvgCmp"
 import { FilterModal } from './FilterModal'
 
 export function LabelsFilter({ onSetFilter, filterBy }) {
-	const [filterByToEdit, setFilterByToEdit] = useState(filterBy)
 	const [showFilterModal, setShowFilter] = useState(false)
 
-	const filterLabels = ['iconic_cities', 'new', 'off-the-grid', 'rooms', 'creative_spaces', 'boats', 'grand_pianos', 'vineyards', 'historical_homes', 'mansions', 'lake', 'bed_&_breakfasts', 'treehouses', 'farms', 'skiing', 'earth_homes', 'countryside', 'amazing_views', 'beach', 'desert', 'a-frames',
+	const filterLabels = ['new', 'off-the-grid', 'iconic_cities', 'rooms', 'creative_spaces', 'boats', 'grand_pianos', 'vineyards', 'historical_homes', 'mansions', 'lake', 'bed_&_breakfasts', 'treehouses', 'farms', 'skiing', 'earth_homes', 'countryside', 'amazing_views', 'beach', 'desert', 'a-frames',
 		'design', 'beachfront', 'caves', 'national_parks', 'castles', 'lakefront', 'islands', 'trulli', 'tropical', 'cabins', 'campers', 'camping', 'arctic', 'tiny_homes', 'surfing', 'barns', 'cycladic_homes', 'hanoks', 'ryokans', 'domes', 'shepard_huts', 'yurts', 'minsus', 'casas_particulares']
 
-	useEffect(() => {
-		onSetFilter(filterByToEdit)
-	}, [filterByToEdit])
-
 	function handleChange(label) {
-		setFilterByToEdit((prevFilterBy) => ({ ...prevFilterBy, ['labels']: [label] }))
+		onSetFilter((prevFilterBy) => ({ ...prevFilterBy, ['labels']: [label] }))
 	}
 
 	const openFilterModal = () => {
