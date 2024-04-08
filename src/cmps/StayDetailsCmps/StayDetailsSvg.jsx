9ba1bf900a6794dd3d64b277824e0e3ec3edc37c
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-09 00:36:48 +0300

    Edit cmp to match new way of importing svgs from service

--- src/cmps/StayDetailsCmps/StayDetailsSvg.jsx
+++ src/cmps/StayDetailsCmps/StayDetailsSvg.jsx
@@ -1,17 +1,6 @@
 import { staySvgService } from '../../services/stay-svg.service'
 import { StringToSvg } from "./StringToSvg"
 
-export function StayDetailsSvg({ bed }) {
-    if (bed === 'couch' || bed === 'sofa bed') {
-        return <span className={`icon ${bed}`} key={bed}><StringToSvg svgString={staySvgService.sofaBed} /></span>
-    }
-    if (bed === 'double bed') {
-        return <span className={`icon ${bed}`} key={bed}><StringToSvg svgString={staySvgService.doubleBed} /></span>
-    }
-    if (bed === 'single bed') {
-        return <span className={`icon ${bed}`} key={bed}><StringToSvg svgString={staySvgService.singleBed} /></span>
-    }
-    if (bed === 'king size bed' || bed === 'queen size bed') {
-        return <span className={`icon ${bed}`} key={bed}><StringToSvg svgString={staySvgService.queenKingBed} /></span>
-    }
+export function StayDetailsSvg({ icon }) {
+        return <span className={`icon ${icon}`} key={icon}><StringToSvg svgString={staySvgService[icon]} /></span>
 }
