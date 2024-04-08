f3e2172ede25aeb5bc3cb41fa6ffbc1bf8b6c796
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-08 21:23:28 +0300

    Add order reducer actions and add it to the store

--- src/store/store.js
+++ src/store/store.js
@@ -8,12 +8,14 @@ import { reservationReducer } from "./reducers/reservation.reducer.js"
 import { userReducer } from "./reducers/user.reducer.js"
 import { reviewReducer } from './reducers/review.reducer.js'
 import { stayReducer } from './reducers/stay.reducer.js'
+import { orderReducer } from './reducers/order.reducer.js'
 
 const rootReducer = combineReducers({
     reservationModule: reservationReducer,
     userModule: userReducer,
     reviewModule: reviewReducer,
-    stayModule: stayReducer
+    stayModule: stayReducer,
+    orderModule: orderReducer
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
