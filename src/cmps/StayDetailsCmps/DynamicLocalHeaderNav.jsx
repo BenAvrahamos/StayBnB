767c8e43b286f6620f3253896ba9fce3bd8e2afb
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-11 15:02:59 +0300

    Add styling properties through classes in cmp

--- src/cmps/StayDetailsCmps/DynamicLocalHeaderNav.jsx
+++ src/cmps/StayDetailsCmps/DynamicLocalHeaderNav.jsx
@@ -1,9 +1,8 @@
 export function DynamicLocalHeaderNav() {
-    return <div className="dynamic-header-nav">
+    return <div className="dynamic-header-nav flex align-center">
         <a href="#gallery">Photos</a>
         <a href="amenities">Amenities</a>
         <a href="reviews">Reviews</a>
         <a href="location">Location</a>
-        //add ids to each section in stay details to catch them when clicking on a in nav.
     </div>
 }
