16cad8e8558ad9c21132404da6416351fc8b0416
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-11 16:53:43 +0300

    Edit cmp

--- src/cmps/StayDetailsCmps/DynamicLocalHeaderNav.jsx
+++ src/cmps/StayDetailsCmps/DynamicLocalHeaderNav.jsx
@@ -1,8 +1,19 @@
+import { useRef } from 'react'
 export function DynamicLocalHeaderNav() {
+
+    function onMoveToSection(e) {
+        e.stopPropagation()
+        const targetElement = e.target
+        const targetPosition = targetElement.getBoundingClientRect().top
+        console.log(targetPosition)
+        const scrollPosition = targetPosition + 30 // Adjust the scroll position to stop 80px before the target
+        window.scrollTo({ top: scrollPosition, behavior: 'smooth' })
+    }
+
     return <div className="dynamic-header-nav flex align-center">
-        <a href="#gallery">Photos</a>
-        <a href="amenities">Amenities</a>
-        <a href="reviews">Reviews</a>
-        <a href="location">Location</a>
+        <a href="#gallery" onClick={onMoveToSection}>Photos</a>
+        <a href="#amenities" onClick={onMoveToSection}>Amenities</a>
+        <a href="#reviews" onClick={onMoveToSection}>Reviews</a>
+        <a href="#location" onClick={onMoveToSection}>Location</a>
     </div>
 }
