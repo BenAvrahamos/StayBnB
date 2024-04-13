c47e4ef35b39e371cf38b0f5e762e42a5b9be471
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-14 01:20:55 +0300

    Update demo data to be stored in storage

--- src/pages/StayIndex.jsx
+++ src/pages/StayIndex.jsx
@@ -11,7 +11,6 @@ import { createNewDemoData } from '../services/data.modification.temp.js'
 
 export function StayIndex({dynamicPageLayOut}) {
     const [searchParams, setSearchParams] = useSearchParams()
-
     const { stays } = useSelector(storeState => storeState.stayModule)
     const { filterBy } = useSelector(storeState => storeState.stayModule)
     const { headerFilterBy } = store.getState().stayModule
@@ -19,7 +18,6 @@ export function StayIndex({dynamicPageLayOut}) {
     useEffect(() => {
         setSearchParams(stayService.mergeFilters(filterBy, headerFilterBy))
         loadStays()
-        createNewDemoData()
     }, [filterBy])
 
     if (!stays || !stays.length) return <section className='index-section'>
