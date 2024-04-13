31b413e6edb590dbdb5ba6e0d3856af1886fe75f
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-14 01:20:33 +0300

    Update stay preview to match new demo data

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -4,13 +4,13 @@ import { utilService } from "../services/util.service"
 export function StayPreview({ stay, filterBy }) {
 
     return <article className="stay-preview">
-        <img src={stay.previewImg}/>
+        <img src={stay.imgUrls[0]}/>
         
         {!filterBy.loc.city && <h1>{stay.loc.city}, {stay.loc.country}</h1>}
-        {filterBy.loc.city && <h1>{stay.name}</h1>}
+        {filterBy.loc.city && <h1>{stay.loc.address.split(', ')[0]}</h1>}
 
         {!filterBy.loc.city && <p className="grayTxt">1,234 kilometers away</p>}
-        {filterBy.loc.city && <p className="grayTxt">{stay.summary}</p>}
+        {filterBy.loc.city && <p className="grayTxt">{stay.summary.length > 34 ? stay.summary.substring(0, 35) + '...' : stay.summary}</p>}
 
         {!filterBy.entryDate && <p className="grayTxt">Apr 7-9</p>}
 
