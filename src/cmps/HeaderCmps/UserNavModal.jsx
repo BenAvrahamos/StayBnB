7b02439d33c55661870d3657013277368c94adac
BenAvrahamos <benavraham1998@gmail.com>
2024-04-13 15:07:50 +0300

    Corrected padding of nav bar

--- src/cmps/HeaderCmps/UserNavModal.jsx
+++ src/cmps/HeaderCmps/UserNavModal.jsx
@@ -6,7 +6,7 @@ export function UserNavModal(){
            <NavLink to="/unActive" className='grayTxt'>Messages</NavLink>
            <NavLink to="/trips" className='grayTxt'>Trips</NavLink>
            <NavLink to="/wishlist" className='grayTxt'>Wishlist</NavLink>
-           <NavLink to="/unActive" className='grayTxt'>Airbnb your home</NavLink>
+           <NavLink to="/edit" className='grayTxt'>Airbnb your home</NavLink>
            <NavLink to="/unActive" className='grayTxt'>Log out</NavLink>
     </section>
 
