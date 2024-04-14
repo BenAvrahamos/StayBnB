0adfea1fd52e497554ee8f6ae7a4bdb15f676153
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-14 22:56:22 +0300

    Edit user reducer and add cmds

--- src/store/reducers/user.reducer.js
+++ src/store/reducers/user.reducer.js
@@ -2,42 +2,41 @@ export const ADD_USER = 'ADD_USER'
 export const REMOVE_USER = 'REMOVE_USER'
 export const SET_USERS = 'SET_USERS'
 export const SET_SCORE = 'SET_SCORE'
-export const SET_USER = 'SET_USER'
-export const SET_USER_SCORE = 'SET_USER_SCORE'
+export const SET_LOGGED_IN_USER = 'SET_LOGGED_IN_USER'
 export const ADD_STAY_TO_FAVORITES = 'ADD_STAY_TO_FAVORITES'
 export const REMOVE_STAY_FROM_FAVORITES = 'REMOVE_STAY_FROM_FAVORITES'
+export const LOGOUT = 'LOGOUT'
 
 
 const initialState = {
-    user: 'user',
     users: [],
+    loggedInUser: '',
+    loggedInUserUserStays: [],
     userFavoriteStays: []
 }
 
 export function userReducer(state = initialState, action = {}) {
     switch (action.type) {
-        case SET_USER:
-            return {
-                ...state,
-                user: action.user
-            }
+        case SET_LOGGED_IN_USER:
+            return {...state, user: action.user}
+
+        case ADD_USER:
+            return {...state, users: [...state.users, action.user]}
+
         case REMOVE_USER:
-            return {
-                ...state,
-                users: state.users.filter(user => user._id !== action.userId)
-            }
+            return {...state, users: state.users.filter(user => user._id !== action.userId)}
+
         case SET_USERS:
             return { ...state, users: action.users }
-        case SET_SCORE:
-            return { ...state, user: { ...state.user, score: action.score } }
-        
-        //Favorite stays
 
         case ADD_STAY_TO_FAVORITES: 
-        return { ...state, userFavoriteStays: [...state.userFavoriteStays, action.stay] } 
+        return { ...state, userFavoriteStays: [...state.userFavoriteStays, action.stayId] } 
 
         case REMOVE_STAY_FROM_FAVORITES: 
-        return { ...state, favoriteStays: state.userFavoriteStays.filter(stay => stay !== action.stayToRemove)}
+        return { ...state, favoriteStays: state.userFavoriteStays.filter(stay => stay._id !== action.stayToRemoveId)}
+
+        case LOGOUT: 
+        return { ...state, loggedInUser: ''}
 
         default:
             return state
