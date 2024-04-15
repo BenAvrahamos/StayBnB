1450b05edc93bca61d6395b268a8c4b479a12690
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-16 01:07:17 +0300

    Add basics to user dashboard

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -1,29 +1,69 @@
 import { userService } from "../services/user.service"
 import { useEffect, useState } from 'react'
 import { orderService } from "../services/order.local.service"
+import { utilService } from "../services/util.service"
+import { stayService } from "../services/stay.service"
 
 export function UserDashboard() {
     const [loggedInUser, setLoggedInUser] = useState(userService.getLoggedInUser())
     const [userOrders, setUserOrders] = useState()
+    const [userStays, setUserStays] = useState()
 
     useEffect(() => {
-        setUserOrders(orderService.getUserOrdersById('000000'))
+        async function getUserOrdersAndStays() {
+            try {
+                const userOrders = await orderService.getUserOrdersById('u101')
+                setUserOrders(userOrders)
+                const _userStays = await stayService.getUserStaysById('ywspv2')
+                setUserStays(_userStays)
+            } catch (err) {
+                console.log(err)
+                throw err
+            }
+        }
+        getUserOrdersAndStays()
     }, [])
+
 
     return (
         <section className="dashboard">
-            {userOrders && <div className="orders-dashboard flex column">
+            {userStays && <div className="user-stays-dashboard">
+                <h2>My stays</h2>
+                <div className="user-stays-container grid">
+                    {userStays.map(stay => {
+                        return <article key={stay._id} className="flex column">
+                            <img src={stay.imgUrls[0]} />
+                            <h2>{stay.name}</h2>
+                            <p><span className="boldTxt">Capacity:</span> {stay.capacity}</p>
+                            <p><span className="boldTxt">Price:</span> {stay.price}</p>
+                            <p><span className="boldTxt">Reviews count:</span> {stay.reviews.length}</p>
+                        </article>
+                    })}
+                </div>
+                </div>}
+            {userOrders && <div className="user-orders-dashboard flex column">
                 <h2>My orders</h2>
-                <div className="orders-container">
-        <div className="headers">
-            <h3>Order number</h3>
-            <h3>Place name</h3>
-            <h3>Guests</h3>
-            <h3>Price</h3>
-            <h3>Dates</h3>
-            <h3>Location</h3>
-        </div>
-       
+                <div className="user-orders-container grid">
+                        <h3 className="order-number">Order number</h3>
+                        <h3 className="place-name">Place name</h3>
+                        <h3 className="guests">Guests</h3>
+                        <h3 className="price">Price</h3>
+                        <h3 className="dates">Dates</h3>
+                        <h3 className="location">Location</h3>
+                    <nav className="user-orders-details">
+                        {userOrders && userOrders.map(order => {
+                            const datesAndGuests = { entryDate: order.entryDate, exitDate: order.exitDate, adults: order.guests.adults, children: order.guests.children }
+                            return <li key={order._orderId} className="user-order flex">
+                                <p>{order._orderId}</p>
+                                <p>{order.stay.name}</p>
+                                <p>{utilService.calcGuestCount(order)}</p>
+                                <p>{Math.round(utilService.calcSumToPay(datesAndGuests, order.stay)) + 
+                                Math.round(utilService.calcSumToPay(datesAndGuests, order.stay) * 0.14125)}</p>
+                                <p>{utilService.timestampsToShortDates(order.entryDate, order.exitDate)}</p>
+                                <p>{order.stay.location}</p>
+                            </li>
+                        })}
+                    </nav>
                 </div>
             </div>}
         </section>
