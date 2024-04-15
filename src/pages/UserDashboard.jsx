05b3558a3e732f5967ecb30b23305f873ad96591
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-15 19:34:26 +0300

    Add basics to user dashboard

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -1,13 +1,43 @@
-import { userService } from "../services/user.service";
-import { useState } from 'react'
+import { userService } from "../services/user.service"
+import { useEffect, useState } from 'react'
+import { orderService } from "../services/order.local.service"
+
 export function UserDashboard() {
     const [loggedInUser, setLoggedInUser] = useState(userService.getLoggedInUser())
-    return (
-        <div className="orders-dashboard flex column">
-            <h2>My orders</h2>
-            <div className="orders-container">
+    const [userOrders, setUserOrders] = useState()
 
-            </div>
+    useEffect(() => {
+        setUserOrders(orderService.getUserOrdersById('000000'))
+    }, [])
+
+    return (
+        <section className="dashboard">
+            {userOrders && <div className="orders-dashboard flex column">
+                <h2>My orders</h2>
+                <div className="orders-container">
+        <div className="headers">
+            <h3>Order number</h3>
+            <h3>Place name</h3>
+            <h3>Guests</h3>
+            <h3>Price</h3>
+            <h3>Dates</h3>
+            <h3>Location</h3>
         </div>
+       
+                </div>
+            </div>}
+        </section>
     )
 }
+
+//  {userOrders.map((order, idx) => {
+//             <div className="order-user-details flex" key={order.buyer._id + idx}>
+//                 <p>55555555</p> 
+//                 {/* change to id from mongo */}
+//                 <p>{order.stay.name}</p>
+//                 <p>{utilService.calcGuestsCount(order)}</p>
+//                 <p>{order.stay.price}</p>
+//                 <p>{utilService.timestampToShortDates(+order.stay.entryDate)}-{utilService.timestampToShortDates(+order.stay.exitDate)}</p>
+//                 <p>{order.stay.loc}</p>
+//                 </div>
+//         })}
