536de51b6e48dd695688095fa462d0522d994c13
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-15 16:20:48 +0300

    Edit order confirmation to get data from the new order

--- src/cmps/StayReservationCmps/OrderConfirmation.jsx
+++ src/cmps/StayReservationCmps/OrderConfirmation.jsx
@@ -1,12 +1,15 @@
-import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import { format, getDay } from 'date-fns'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { utilService } from '../../services/util.service'
 
-export function OrderConfirmation({ stay, params }) {
+export function OrderConfirmation({ stay, order }) {
     const [isShownModal, setIsShownModal] = useState(true)
-    const reservation = useSelector(storeState => storeState.reservationModule.reservation)
+    const sumToPay = Math.round(utilService.calcSumToPay({
+        entryDate: order.entryDate,
+        exitDate: order.exitDate,
+        adults: order.guests.adults,
+        children: order.guests.children
+    }, stay))
     const navigate = useNavigate()
 
     function onCloseModal(e) {
@@ -15,85 +18,93 @@ export function OrderConfirmation({ stay, params }) {
         navigate('/trips')
     }
 
+    function calcGuestCount() {
+        let sumOfGuests = 0
+        for (let guestTypeCount in order.guests) {
+            sumOfGuests += +order.guests[guestTypeCount]
+        }
+        return sumOfGuests
+    }
+
     return (
         isShownModal &&
         <>
             <div className='overlay' onClick={onCloseModal}></div>
             <section className="order-confirmation">
-                    <header className="order-header flex center">
-                        <button onClick={onCloseModal} className='exit-btn'>X</button>
-                        <h1 className="order-reserved">Reservation confirmed</h1>
-                    </header>
+                <header className="order-header flex center">
+                    <button onClick={onCloseModal} className='exit-btn'>X</button>
+                    <h1 className="order-reserved">Reservation confirmed</h1>
+                </header>
 
-                    <div className='order-details grid'>
+                <div className='order-details grid'>
                     <img src={stay.imgUrls[0]} title={stay.name} />
 
-                        <h1>Reservation details</h1>
+                    <h1>Reservation details</h1>
 
-                        <div className='user flex column'>
-                            <p>Guest name:</p>
-                            <h5>ADD USER NAME HERE</h5>
+                    <div className='user flex column'>
+                        <p>Guest name:</p>
+                        <h5>{order.buyer.fullname}</h5>
+                    </div>
+
+                    <div className='stay-name flex column'>
+                        <p>Property name:</p>
+                        {/* <h5>{stay.name}</h5> */}
+                        <h5>{stay.propertyType} in {stay.loc.city}, {stay.loc.country}</h5>
+                    </div>
+
+                    <div className='booking-number flex column'>
+                        <p>Booking number:</p>
+                        <h5>{order._id}</h5>
+                    </div>
+
+                    <div className='dates grid'>
+                        <div className='flex column'>
+                            <p>Check-in:</p>
+                            <h5>{utilService.timestampToDate(+order.entryDate)}</h5>
                         </div>
 
-                        <div className='stay-name flex column'>
-                            <p>Property name:</p>
-                            {/* <h5>{stay.name}</h5> */}
-                            <h5>{stay.propertyType} in {stay.loc.city}, {stay.loc.country}</h5>
-                        </div>
-
-                        <div className='booking-number flex column'>
-                            <p>Booking number:</p>
-                            <h5>ADD ORDER NUMBER HERE</h5>
-                        </div>
-
-                        <div className='dates grid'>
-                            <div className='flex column'>
-                                <p>Check-in:</p>
-                                <h5>{utilService.timestampToDate(+params.entryDate)}</h5>
-                            </div>
-
-                            <div className='flex column'>
-                                <p>Check-out:</p>
-                                <h5>{utilService.timestampToDate(+params.exitDate)}</h5>
-                            </div>
-                        </div>
-
-                        <div className='guests-rooms grid'>
-                            <div className='guests-container flex column'>
-                                <div className='guests flex'>
-                                    <p>Total guests:</p>
-                                    <h5>{reservation.guests.sum}</h5>
-                                </div>
-                                <ul>
-                                    {+params.adults > 0 && <li>{+params.adults === 1 ? `${reservation.guests.sum} adult` : `${reservation.guests.sum} adults`}</li>}
-                                    {+params.children > 0 && <li>{+params.children === 1 ? `${reservation.guests.sum} child` : `${reservation.guests.sum} children`}</li>}
-                                    {+params.infants > 0 && <li>{+params.infants === 1 ? `${reservation.guests.sum} infant` : `${reservation.guests.sum} infants`}</li>}
-                                    {+params.pets > 0 && <li>{+params.pets === 1 ? `${reservation.guests.sum} pet` : `${reservation.guests.sum} pets`}</li>}
-                                </ul>
-                            </div>
-
-                            <div className='rooms flex column'>
-                                <p>Total Rooms:</p>
-                                <h5>{stay.bedrooms.length}</h5>
-                            </div>
-                        </div>
-
-                        <div className='price flex column'>
-                            <div className='flex space-between'>
-                                <p>Price:</p>
-                                <p>${stay.price} X {utilService.calcSumOfDays(params)} nights &nbsp;&nbsp;&nbsp;&nbsp;
-                                    <span>${(Math.round(utilService.calcSumToPay(params, stay)))}</span></p>
-                            </div>
-                            <div className='flex space-between'>
-                                <p>Service fee:</p>
-                                <p>${(Math.round(utilService.calcSumToPay(params, stay) * 0.14125))}</p>
-                            </div>
-                            <div className='flex space-between'>
-                                <h4>Total:</h4>
-                                <p><span>${(Math.round(utilService.calcSumToPay(params, stay)) + (Math.round(utilService.calcSumToPay(params, stay) * 0.14125)))}</span></p>
-                            </div>
+                        <div className='flex column'>
+                            <p>Check-out:</p>
+                            <h5>{utilService.timestampToDate(+order.exitDate)}</h5>
                         </div>
                     </div>
+
+                    <div className='guests-rooms grid'>
+                        <div className='guests-container flex column'>
+                            <div className='guests flex'>
+                                <p>Total guests:</p>
+                                <h5>{calcGuestCount()}</h5>
+                            </div>
+                            <ul>
+                                {+order.adults > 0 && <li>{+order.adults === 1 ? `${order.guests.adults} adult` : `${order.guests.adults} adults`}</li>}
+                                {+order.children > 0 && <li>{+order.children === 1 ? `${order.guests.children} child` : `${order.guests.children} children`}</li>}
+                                {+order.infants > 0 && <li>{+order.infants === 1 ? `${order.guests.infants} infant` : `${order.guests.infants} infants`}</li>}
+                                {+order.pets > 0 && <li>{+order.pets === 1 ? `${order.guests.pets} pet` : `${order.guests.pets} pets`}</li>}
+                            </ul>
+                        </div>
+
+                        <div className='rooms flex column'>
+                            <p>Total Rooms:</p>
+                            <h5>{stay.bedrooms.length}</h5>
+                        </div>
+                    </div>
+
+                    <div className='price flex column'>
+                        <div className='flex space-between'>
+                            <p>Price:</p>
+                            <p>${stay.price} X {utilService.calcSumOfDays({entryDate: order.entryDate, exitDate: order.exitDate})} nights &nbsp;&nbsp;&nbsp;&nbsp;
+                                <span>${Math.round(sumToPay)}</span></p>
+                        </div>
+                        <div className='flex space-between'>
+                            <p>Service fee:</p>
+                            <p>${Math.round(sumToPay * 0.14125)}</p>
+                        </div>
+                        <div className='flex space-between'>
+                            <h4>Total:</h4>
+                            <p><span>${Math.round(sumToPay) + Math.round(sumToPay * 0.14125)}</span></p>
+                        </div>
+                    </div>
+                </div>
 
                 <footer className='flex center'>
                     <button className="close-btn flex center" onClick={onCloseModal}>Close</button>
