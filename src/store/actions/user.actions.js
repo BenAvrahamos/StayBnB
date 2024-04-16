31f97cc69266a9df2a1530f29b87b0b90825e1ba
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-16 16:51:53 +0300

    Change action imports and functions to match service of backend

--- src/store/actions/user.actions.js
+++ src/store/actions/user.actions.js
@@ -1,14 +1,12 @@
 import { storageService } from '../../services/async-storage.service'
-import { userService } from '../../services/user.local.service'
+import { userService } from '../../services/user.service'
 import { utilService } from '../../services/util.service'
 import { ADD_STAY_TO_FAVORITES, SET_LOGGED_IN_USER, SET_USERS, REMOVE_STAY_FROM_FAVORITES, ADD_USER, LOGOUT } from "../reducers/user.reducer"
 import { store } from "../store"
 
-const LOGGEDINUSER_DB = 'loggedinuser_db'
-
 export async function loadUsers() {
     try {
-        const users = await userService.query(USER_DB)
+        const users = await userService.query()
         store.dispatch({ type: SET_USERS, users })
     } catch (err) {
         console.log('stay action -> Cannot load stays', err)
@@ -19,22 +17,20 @@ export async function loadUsers() {
 export async function login(credentials) {
     try {
         const user = await userService.login(credentials)
-        store.dispatch(SET_LOGGED_IN_USER, user)
+        store.dispatch( {type: SET_LOGGED_IN_USER, user} )
+        return user
     } catch (err) {
         console.log('err', err)
     }
 }
 
-export async function signup(userInfo) {
+export async function signup(credentials) {
     try {
-        const users = await userService.query()
-        const isUser = users.find(user => user.username === userInfo.username)
-        if (!isUser) {
-            const userToLogIn = await userService.signup(userInfo)
-            store.dispatch({ ADD_USER, user: userToLogIn })
-            store.dispatch({ SET_LOGGED_IN_USER, user: userToLogIn })
-        }
-    } catch (err) {
+        const user = await userService.signup(credentials)
+            store.dispatch({ type: ADD_USER, user: user })
+            store.dispatch({ type: SET_LOGGED_IN_USER, user: user })
+            return user
+        } catch (err) {
         console.log(err)
         throw err
     }
@@ -43,7 +39,7 @@ export async function signup(userInfo) {
 export async function logout() {
     try {
         await userService.logout()
-        store.dispatch({ LOGOUT })
+        store.dispatch({ type: LOGOUT })
     } catch (err) {
         console.log(err)
         throw err
@@ -54,7 +50,7 @@ export async function addStayToUserFavorites(stayId) {
     try {
         if (userService.getLoggedInUser()) {
             userService.addStayToUserFavorites(stayId)
-            store.dispatch({ ADD_STAY_TO_FAVORITES, stayId })
+            store.dispatch({ type: ADD_STAY_TO_FAVORITES, stayId })
         }
     } catch (err) {
         console.log('stay action -> Cannot save stay', err)
@@ -64,7 +60,7 @@ export async function addStayToUserFavorites(stayId) {
 
 export async function removeStayToUserFavorites(stayId) {
     try {
-        if (userService.getLoggedInUser()) store.dispatch({ REMOVE_STAY_FROM_FAVORITES, stayId })
+        if (userService.getLoggedInUser()) store.dispatch({ type: REMOVE_STAY_FROM_FAVORITES, stayId })
     } catch (err) {
         console.log('stay action -> Cannot save stay', err)
         throw err
