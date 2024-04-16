970d44e5328f152e3235d8d9f9735e6994f7d459
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-16 13:25:48 +0300

    Edit usernavmodal to include loginsignup and add basics to show dynamically relevant links

--- src/cmps/HeaderCmps/UserNavModal.jsx
+++ src/cmps/HeaderCmps/UserNavModal.jsx
@@ -1,15 +1,34 @@
 import { NavLink } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { userService } from '../../services/user.service'
+import { login } from '../../store/actions/user.actions'
 
-export function UserNavModal(){
+export function UserNavModal() {
+    const [isLoggedInUser, checkIsLoggedInUser] = useState()
+
+    async function onGuestClick() {
+        try {
+            const user = login({ username: 'guest', password: 'guest' })
+            if (user) checkIsLoggedInUser(true)
+        } catch (err) {
+            console.log(err)
+            throw err
+        }
+    }
+    
+    useEffect(() => {
+        checkIsLoggedInUser(Boolean(userService.getLoggedInUser()))
+    })
 
     return <section className="user-nav-modal" >
-        <NavLink to="/loginsignup" className="grayTxt">Login / Signup</NavLink>
-           <NavLink to="/unActive" className='grayTxt'>Messages</NavLink>
-           <NavLink to="/trips" className='grayTxt'>Trips</NavLink>
-           <NavLink to="/wishlist" className='grayTxt'>Wishlist</NavLink>
-           <NavLink to="/edit" className='grayTxt'>Airbnb your home</NavLink>
-           <NavLink to="/dashboard" className='grayTxt'>Dashboard</NavLink>
-           <NavLink to="/unActive" className='grayTxt'>Log out</NavLink>
+        {!isLoggedInUser && <NavLink to="/loginsignup" className="grayTxt">Login / Signup</NavLink>}
+        {!isLoggedInUser && <NavLink to="/" onClick={() => onGuestClick()}>Continue as Guest</NavLink>}
+        <NavLink to="/unActive" className='grayTxt'>Messages</NavLink>
+        <NavLink to="/trips" className='grayTxt'>Trips</NavLink>
+        <NavLink to="/wishlist" className='grayTxt'>Wishlist</NavLink>
+        <NavLink to="/edit" className='grayTxt'>Airbnb your home</NavLink>
+        <NavLink to="/dashboard" className='grayTxt'>Dashboard</NavLink>
+        {isLoggedInUser && <NavLink to="/unActive" className='grayTxt'>Log out</NavLink>}
     </section>
 
 }
