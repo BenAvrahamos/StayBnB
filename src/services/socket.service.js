955dd7aa3daae897230527830db650953734a8e4
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-17 17:40:05 +0300

    Add event to socket service

--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -8,8 +8,8 @@ export const SOCKET_EMIT_USER_WATCH = 'user-watch'
 export const SOCKET_EVENT_USER_UPDATED = 'user-updated'
 export const SOCKET_EVENT_REVIEW_ADDED = 'review-added'
 export const SOCKET_EVENT_REVIEW_ABOUT_YOU = 'review-about-you'
-export const SOCKET_EVENT_ODER_UPDATE = 'order-update'
-
+export const SOCKET_EVENT_ORDER_UPDATE = 'order-update'
+export const SOCKET_SERVICE_NOTIFICATION = 'prompt-notification'
 const SOCKET_EMIT_LOGIN = 'set-user-socket'
 const SOCKET_EMIT_LOGOUT = 'unset-user-socket'
 
